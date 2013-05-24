35bf4ca2d2d83e3fa97ed1641df9cfc5f084fea9
Jere <Jere@Jere-PC>
2013-05-23 23:44:36 -0300

    Completo sin JPA

---
+++ src/ventaporcatalogo/VentaPorCatalogo.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ventaporcatalogo;
+
+/**
+ *
+ * @author Jere
+ */
+public class VentaPorCatalogo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
